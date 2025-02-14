e92ea7d03c6493b1187f46c21b93f447303e0fc8
sfortz <sophie.fortz@unamur.be>
2025-02-14 15:54:14 +0100

    Create BundleEventStructureElementPrinter.java

---
+++ src/main/java/uk/kcl/info/bfm/io/xml/BundleEventStructureElementPrinter.java
@@ -0,0 +1,26 @@
+package uk.kcl.info.bfm.io.xml;
+
+import uk.kcl.info.bfm.BundleEventStructure;
+import uk.kcl.info.bfm.CausalityRelation;
+import uk.kcl.info.bfm.ConflictRelation;
+import uk.kcl.info.bfm.Event;
+import java.util.Iterator;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+public interface BundleEventStructureElementPrinter {
+    void printElement(XMLStreamWriter writer, BundleEventStructure bes) throws XMLStreamException;
+
+    void printEvents(XMLStreamWriter writer, Iterator<Event> events) throws XMLStreamException;
+
+    void printElement(XMLStreamWriter writer, Event event) throws XMLStreamException;
+
+    void printCausalities(XMLStreamWriter writer, Iterator<CausalityRelation> causalities) throws XMLStreamException;
+
+    void printElement(XMLStreamWriter writer, CausalityRelation causality) throws XMLStreamException;
+
+    void printConflicts(XMLStreamWriter writer, Iterator<ConflictRelation> conflicts) throws XMLStreamException;
+
+    void printElement(XMLStreamWriter writer, ConflictRelation conflict) throws XMLStreamException;
+
+}
