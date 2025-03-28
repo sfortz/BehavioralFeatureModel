8cbf2f2baa9a4a0106e521207bb73da091715353
sfortz <sophie.fortz@unamur.be>
2025-03-28 09:51:53 +0100

    Example update

--- src/main/resources/ts/new.ts
+++ src/main/resources/ts/new.ts
@@ -3,39 +3,39 @@
     <start>State_0</start>
     <states>
         <state id="State_2">
-            <transition target="State_1" action="clean"></transition>
+            <transition target="State_7" action="goAround"></transition>
         </state>
         <state id="State_3">
-            <transition target="State_9" action="goAround"></transition>
+            <transition target="State_5" action="clean"></transition>
         </state>
-        <state id="State_10">
-            <transition target="State_4" action="charge"></transition>
+        <state id="State_10"></state>
+        <state id="State_4">
+            <transition target="State_6" action="goAround"></transition>
         </state>
-        <state id="State_4"></state>
-        <state id="State_11">
-            <transition target="State_6" action="charge"></transition>
+        <state id="State_11"></state>
+        <state id="State_5">
+            <transition target="State_10" action="charge"></transition>
         </state>
-        <state id="State_5"></state>
-        <state id="State_12">
-            <transition target="State_3" action="caDet"></transition>
-            <transition target="State_7" action="liDet"></transition>
-            <transition target="State_8" action="move"></transition>
-        </state>
+        <state id="State_12"></state>
         <state id="State_0">
-            <transition target="State_12" action="map"></transition>
+            <transition target="State_1" action="map"></transition>
         </state>
         <state id="State_1">
-            <transition target="State_5" action="charge"></transition>
+            <transition target="State_2" action="caDet"></transition>
+            <transition target="State_3" action="move"></transition>
+            <transition target="State_4" action="liDet"></transition>
         </state>
-        <state id="State_6"></state>
+        <state id="State_6">
+            <transition target="State_8" action="clean"></transition>
+        </state>
         <state id="State_7">
-            <transition target="State_2" action="goAround"></transition>
+            <transition target="State_9" action="clean"></transition>
         </state>
         <state id="State_8">
-            <transition target="State_10" action="clean"></transition>
+            <transition target="State_11" action="charge"></transition>
         </state>
         <state id="State_9">
-            <transition target="State_11" action="clean"></transition>
+            <transition target="State_12" action="charge"></transition>
         </state>
     </states>
 </ts>
